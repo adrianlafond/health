83eb1e2828aee45781f7052ebd06716bed5cf196
Adrian Lafond <mail@adrianlafond.com>
2024-06-09 22:48:32 -0400

    Initial setup (#1)
    * install vite + typescript + svelte
    
    * install tailwindcss
    
    * install ts-standard
    
    * trade ts-standed for eslint-plugin-svelte
    
    * get vitest working
    
    * fail to get eslint working

---
+++ vite.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelteTesting } from '@testing-library/svelte/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte(), svelteTesting()],
+  test: {
+    environment: 'happy-dom',
+    globals: true,
+    setupFiles: ['./vitest-setup.js'],
+  }
+})

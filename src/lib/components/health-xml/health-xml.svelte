f04e729b9f5a21d873cd64ebe803e7e78407d6d4
Adrian Lafond <mail@adrianlafond.com>
2024-09-01 15:58:41 -0400

    show loading or processing state

--- src/lib/components/health-xml/health-xml.svelte
+++ src/lib/components/health-xml/health-xml.svelte
@@ -1,16 +1,24 @@
 <script>
   import { enhance } from '$app/forms'
+
+  let processing = false
 </script>
 
 <form
   method="POST"
   action="/apple?/upload"
-  use:enhance
+  use:enhance={() => {
+    processing = true
+    return async ({ update }) => {
+      await update()
+      processing = false
+    }
+  }}
   enctype="multipart/form-data"
 >
   <label>
-    Apple Health "export.xml"
+    {processing ? 'Processing' : 'Apple Health "export.xml"'}
     <input name="file" type="file" accept="xml" />
   </label>
-  <button type="submit">Load data</button>
+  <button disabled={processing} type="submit">Load data</button>
 </form>
--- src/routes/apple/+page.svelte
+++ src/routes/apple/+page.svelte
@@ -3,18 +3,30 @@
   import HealthXml from '$lib/components/health-xml/health-xml.svelte'
 
   export let form: HealthResponse
+
+  console.log('form:', form)
+  let loading = false
 </script>
 
 <h1>/apple</h1>
 
-<HealthXml />
+<HealthXml on:submit={() => (loading = true)} />
 
-{#if form?.data.error === false}
-  <p>{form?.data.date}</p>
+{#if form?.data.error}
+  <p>{form.data.message}</p>
+{:else if form?.data.error === false}
+  <p>{form.data.date}</p>
   <p>{form.data.user.bloodType}</p>
   <p>{form.data.user.gender}</p>
   <p>
     {form.data.user.dob?.year}-{form.data.user.dob?.month}-{form.data.user.dob
       ?.date}
   </p>
+  {#each form.data.bloodPressure as bloodPressure}
+    <p>
+      {bloodPressure.date}: {bloodPressure.systolic} / {bloodPressure.diastolic}
+    </p>
+  {/each}
+{:else if loading}
+  <p>Loading...</p>
 {/if}
